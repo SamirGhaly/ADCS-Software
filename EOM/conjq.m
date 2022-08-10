97215d1c5755ba27b748f36bf62e0220a8900f27
Samir Samy <90931412+iMonGeZ@users.noreply.github.com>
2022-08-10 11:37:42 +0200

    Delete EOM directory

--- EOM/conjq.m
+++
@@ -1,5 +0,0 @@
-function q_telda=conjq(q)
-
-q_telda=[q(1) -q(2) -q(3) -q(4)];
-
-end
--- EOM/newfrm.m
+++
@@ -1,6 +0,0 @@
-function V_NewFrame=newfrm(v,q)
-
-v_old=[0 v(1) v(2) v(3)];  %As Quaternion
-V=quatmul(quatmul(conjq(q),v_old),q);
-V_NewFrame=[V(2) V(3) V(4)]'; %Coloumn
-end
